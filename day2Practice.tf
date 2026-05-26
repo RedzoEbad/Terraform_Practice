d9cdb6b26de46e2fcb304c0740acac4e2fe09e43
RedzoEbad <ebadkhan6292002@gmail.com>
2026-05-26 23:23:34 +0500

    upda

--- day2Practice.tf
+++ day2Practice.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "s3" {
+    bucket = "mybucketbadkhan1234323"
+    key    = "dev/terraform.tfstate"
+    region = "eu-north-1"
+    use_lockfile = true 
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -13,11 +19,41 @@ provider "aws" {
   region = "eu-north-1"
 }
 
+
 resource "aws_s3_bucket" "example123" {
   bucket = "my-tf-test-bucket-124324343254324343434352334"
 
   tags = {
     Name        = "My bucket-12344"
-    Environment = "Dev1231234"
+    Environment = "DEV"
+  }
+}
+
+variable "env" {
+  default = "dev"
+  type = string
+}
+
+locals {
+  env = var.env
+
+}
+
+resource "aws_s3_bucket" "example1234" {
+  bucket = "my-tf-test-bucket-12432434325432434343435233422"
+
+  tags = {
+    Name        = "My bucket-12344"
+    Environment = locals.env
+  }
+}
+
+
+resource "aws_s3_bucket" "example1234" {
+  bucket = "my-tf-test-bucket-12432434325432434343435233422"
+
+  tags = {
+    Name        = "My bucket-12344"
+    Environment =  locals.env
   }
 }
